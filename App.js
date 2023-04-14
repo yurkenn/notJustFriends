2d0cfe9645ec0c50478c9ce288ce8a1c98fbddcb
oğuz yürken <oguz.yurken@gmail.com>
2023-04-14 21:35:44 +0300

    Add navigator

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, SafeAreaView, StyleSheet, View } from 'react-native';
-import FeedPost from './src/components/FeedPost';
-import posts from './assets/data/posts.json';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import Navigator from './src/navigation';
 
 export default function App() {
-  const renderItem = ({ item }) => <FeedPost post={item} />;
-
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList data={posts} renderItem={renderItem} />
+      <Navigator />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -17,9 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 50,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
