998a098ecfbd04f40dab2597324207bb0b0ce485
oğuz yürken <oguz.yurken@gmail.com>
2023-04-16 19:19:47 +0300

    Add models

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "notJustFriends": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "notJustFriends": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3notjustfriendsstorage6ae166bc": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
